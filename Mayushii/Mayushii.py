859cbdb474c43816bc74356917371bf3519e2484
Cave Johnson <hendrik.henderson@gmail.com>
2018-02-20 13:22:29 -0600

    Update Mayushii.py

--- Mayushii/Mayushii.py
+++ Mayushii/Mayushii.py
@@ -36,11 +36,11 @@ class Mayushii:
                 "Fun fact: My favourite food is chicken tenders, specifically 'Juicy Karaage Number 1'!",
                 ("We should work towards world peace."
                     "\nLike giving everyone in the world an Upa cushion...")],
-            "I love you" : ["Mayushii☆ is flattered but she would rather stay friends (⌒_⌒;) "],
+            "i love you" : ["Mayushii☆ is flattered but she would rather stay friends (⌒_⌒;) "],
             "birthday" : [("Hm, well, *my* birthday is February 1st, 1994, so I guess that makes me 24 as of 2018! "
                            "\nBut on the other hand, Mayushii☆ is an A.I., so that makes me kinda ageless (⌒_⌒;) "
                            "\nMayushii☆'s first message was on Sunday, February 18th, 2018, at 12:45 A.M U.S. Central. ")],
-            "blood type" : [("My blood type is O! (* ^ ω ^) ",
+            "blood type" : [("My blood type is O! (* ^ ω ^) "
                             "\nI think that means I'm supposed to be 'confident, self-determined, strong-willed and intuitive' "
                             "\nand also 'self-centered, cold, unpredictable and a workaholic', "
                             "\nbut I don't really think that's true. -_-")],
